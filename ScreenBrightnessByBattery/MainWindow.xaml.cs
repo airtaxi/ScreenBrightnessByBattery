74d500611d7f58029417ba1883268d101bd0ad56
Howon Lee <kck4156@kagamine-rin.com>
2024-08-21 00:39:32 +0900

    Bugfux & Added support for auto brightness & Refactor

--- ScreenBrightnessByBattery/MainWindow.xaml.cs
+++ ScreenBrightnessByBattery/MainWindow.xaml.cs
@@ -20,17 +20,32 @@ public sealed partial class MainWindow : Window
     private const string SettingsKeyBattery = "Battery";
     private const string SettingsKeyAc = "AC";
 
-    // Path to the settings file
+    /// <summary>
+    /// Path to the settings file
+    /// </summary>
     private static readonly string SettingsPath = Path.Combine(AppContext.BaseDirectory, "settings.ini");
+
+    /// <summary>
+    /// 1500ms is adequate interval.
+    /// Change it if you want
+    /// </summary>
+    private static readonly System.Timers.Timer SaveCurrentBrightnessSettingsTimer = new(1500);
+
+    static MainWindow()
+    {
+        SaveCurrentBrightnessSettingsTimer.Elapsed += async (s, e) =>
+        {
+            await ApplySettingsAsync();
+            SaveCurrentBrightnessSettings();
+        };
+        SaveCurrentBrightnessSettingsTimer.Start();
+    }
 
     public MainWindow()
     {
         InitializeComponent();
 
         AppWindow.IsShownInSwitchers = false;
-
-        var battery = Battery.AggregateBattery;
-        battery.ReportUpdated += OnBatteryReportUpdated;
 
         if(!File.Exists(SettingsPath))
         {
@@ -39,64 +54,103 @@ public sealed partial class MainWindow : Window
         }
 
         UpdateStartupProcessMenuFlyoutItemText();
-        ApplySettings();
     }
 
-    // Indicates if the last time we checked the power supply status, we were on battery or not
-    // Null means we haven't checked yet
+    /// <summary>
+    /// Indicates if the last time we checked the power supply status, we were on battery or not
+    /// Null means we haven't checked yet
+    /// </summary>
     private static bool? s_wasOnBattery;
-    private static SemaphoreSlim s_applyBrightnessSemaphre = new(1, 1);
 
     /// <summary>
     /// Applies the brightness settings based on the current power supply status
     /// Also saves the current brightness settings to the settings file
     /// </summary>
-    private static async void ApplySettings()
+    private static async Task ApplySettingsAsync()
     {
-        await s_applyBrightnessSemaphre.WaitAsync();
-        try
+        if (IsOnBattery)
         {
-            var status = PowerManager.PowerSupplyStatus;
-            SaveCurrentBrightnessSettings();
+            // If we were on battery last time, do nothing
+            if (s_wasOnBattery == true) return;
 
-            if (status == PowerSupplyStatus.NotPresent)
+            // Get the brightness from the settings file
+            var rawBrightness = IniFile.GetValue(SettingsPath, SettingsSection, SettingsKeyBattery, SettingsDefaultRawBatteryBrightness);
+
+            // Apply the brightness settings if raw brightness is auto
+            if (rawBrightness == "auto")
             {
-                // (Widows Bug?) This method is called multiple times when device is on battery
-                if (s_wasOnBattery == true) return;
-
-                // (Widows bug?) Setting brigntness immediately after getting brightness is glitchy
-                await Task.Delay(1000);
-
-                // Get the brightness from the settings file
-                var rawBrightness = IniFile.GetValue(SettingsPath, SettingsSection, SettingsKeyBattery, SettingsDefaultRawBatteryBrightness);
-                var success = int.TryParse(rawBrightness, out int brightness);
-                if (!success) brightness = int.Parse(SettingsDefaultRawBatteryBrightness);
-
-                // Apply the brightness settings
-                Debug.WriteLine($"Applying battery brightness {brightness}");
-                WindowsSettingsBrightnessController.Set(brightness);
+                Debug.WriteLine("Applying battery brightness: auto");
+                SetAdaptiveBrightness(true);
                 s_wasOnBattery = true;
+                return;
             }
-            else if (status == PowerSupplyStatus.Adequate || status == PowerSupplyStatus.Inadequate)
+            // Turn off adaptive brightness if it is enabled
+            else
             {
-                // (Widows Bug?) This method is called multiple times when device is on AC
-                if (s_wasOnBattery == false) return;
-
-                // (Widows bug?) Setting brigntness immediately after getting brightness is glitchy
-                await Task.Delay(100);
-
-                // Get the brightness settings from the settings file
-                var rawBrightness = IniFile.GetValue(SettingsPath, SettingsSection, SettingsKeyAc, SettingsDefaultRawAcBrightness);
-                var success = int.TryParse(rawBrightness, out int brightness);
-                if (!success) brightness = int.Parse(SettingsDefaultRawAcBrightness);
-
-                // Apply the brightness settings
-                Debug.WriteLine($"Applying AC brightness {brightness}");
-                WindowsSettingsBrightnessController.Set(brightness);
-                s_wasOnBattery = false;
+                var isAdaptiveBrightnessEnabled = GetAdaptiveBrightnessStatus(false);
+                if (isAdaptiveBrightnessEnabled == true) SetAdaptiveBrightness(false);
             }
+
+            // Otherwise, apply the brightness settings
+            var success = int.TryParse(rawBrightness, out int brightness);
+            if (!success) brightness = int.Parse(SettingsDefaultRawBatteryBrightness);
+
+            // Apply the brightness settings
+            Debug.WriteLine($"Applying battery brightness {brightness}");
+
+            // Bugfix: Set different two brightness values to force the brightness to change
+            // (Maybe on surface devices. My device has this issue)
+            if (brightness > 50) PowerConfigBrightnessController.Set(25);
+            else PowerConfigBrightnessController.Set(75);
+
+            // Wait for a while to let the brightness change (500ms is adequate)
+            await Task.Delay(500);
+
+            // Set the actual brightness
+            PowerConfigBrightnessController.Set(brightness);
+            s_wasOnBattery = true;
         }
-        finally { s_applyBrightnessSemaphre.Release(); }
+        else
+        {
+            // If we were on AC last time, do nothing
+            if (s_wasOnBattery == false) return;
+
+            // Get the brightness settings from the settings file
+            var rawBrightness = IniFile.GetValue(SettingsPath, SettingsSection, SettingsKeyAc, SettingsDefaultRawAcBrightness);
+            // Apply the brightness settings if raw brightness is auto
+            if (rawBrightness == "auto")
+            {
+                Debug.WriteLine("Applying AC brightness: auto");
+                SetAdaptiveBrightness(true);
+                s_wasOnBattery = false;
+                return;
+            }
+            // Turn off adaptive brightness if it is enabled
+            else
+            {
+                var isAdaptiveBrightnessEnabled = GetAdaptiveBrightnessStatus(true);
+                if (isAdaptiveBrightnessEnabled == true) SetAdaptiveBrightness(false);
+            }
+
+            // Otherwise, apply the brightness settings
+            var success = int.TryParse(rawBrightness, out int brightness);
+            if (!success) brightness = int.Parse(SettingsDefaultRawAcBrightness);
+
+            // Apply the brightness settings
+            Debug.WriteLine($"Applying AC brightness {brightness}");
+
+            // Bugfix: Set different two brightness values to force the brightness to change
+            // (Maybe on surface devices. My device has this issue)
+            if (brightness > 50) PowerConfigBrightnessController.Set(25);
+            else PowerConfigBrightnessController.Set(75);
+
+            // Wait for a while to let the brightness change (500ms is adequate)
+            await Task.Delay(500);
+
+            // Set the actual brightness
+            PowerConfigBrightnessController.Set(brightness);
+            s_wasOnBattery = false;
+        }
     }
 
     /// <summary>
@@ -104,43 +158,175 @@ public sealed partial class MainWindow : Window
     /// </summary>
     private static void SaveCurrentBrightnessSettings()
     {
-        // Save the current brightness if we were on battery
-        if (s_wasOnBattery == true)
+        // Save the current brightness if are on battery
+        if (IsOnBattery && s_wasOnBattery == true)
         {
-            var currentBrightness = WindowsSettingsBrightnessController.Get();
-            Debug.WriteLine($"Saving battery brightness {currentBrightness}");
+            // If adaptive brightness is enabled, save the brightness as auto
+            var isAdaptiveBrightnessEnabled = GetAdaptiveBrightnessStatus(false);
+            if(isAdaptiveBrightnessEnabled == true)
+            {
+                Debug.WriteLine("Saving battery brightness: auto");
+                IniFile.SetValue(SettingsPath, SettingsSection, SettingsKeyBattery, "auto");
+                return;
+            }
+
+            // Otherwise, save the current brightness
+            var currentBrightness = PowerConfigBrightnessController.Get(!IsOnBattery);
+            Debug.WriteLine($"Saving battery brightness: {currentBrightness}");
             IniFile.SetValue(SettingsPath, SettingsSection, SettingsKeyBattery, currentBrightness.ToString());
         }
-        // Save the current brightness if we were on AC
-        else if (s_wasOnBattery == false)
+        // Save the current brightness if we are on AC
+        else if(!IsOnBattery && s_wasOnBattery == false)
         {
-            var currentBrightness = WindowsSettingsBrightnessController.Get();
-            Debug.WriteLine($"Saving AC brightness {currentBrightness}");
+            // If adaptive brightness is enabled, save the brightness as auto
+            var isAdaptiveBrightnessEnabled = GetAdaptiveBrightnessStatus(true);
+            if (isAdaptiveBrightnessEnabled == true)
+            {
+                Debug.WriteLine("Saving AC brightness: auto");
+                IniFile.SetValue(SettingsPath, SettingsSection, SettingsKeyAc, "auto");
+                return;
+            }
+
+            // Otherwise, save the current brightness
+            var currentBrightness = PowerConfigBrightnessController.Get(!IsOnBattery);
+            Debug.WriteLine($"Saving AC brightness: {currentBrightness}");
             IniFile.SetValue(SettingsPath, SettingsSection, SettingsKeyAc, currentBrightness.ToString());
         }
     }
+
+    /// <summary>
+    /// Indicates if the device is currently on battery
+    /// </summary>
+    private static bool IsOnBattery => PowerManager.PowerSupplyStatus == PowerSupplyStatus.NotPresent;
 
     /// <summary>
     /// Updates the text of the Startup Process Menu Flyout Item
     /// </summary>
     private void UpdateStartupProcessMenuFlyoutItemText()
     {
+        // Get the menu flyout item
         var menuFlyoutItem = MfiStartupProcess;
+
+        // Update the text of the menu flyout item based on the current startup process status
         menuFlyoutItem.Text = StartupProcessHelper.IsStartupProcess ? "Remove from Startup Process" : "Add to Startup Process";
     }
 
-    private void OnBatteryReportUpdated(Battery sender, object args) => ApplySettings();
-
     // Menu Flyout Item Click Handlers
-    private void OnOpenSettingsFileMenuFlyoutItemClicked(object sender, RoutedEventArgs e) => System.Diagnostics.Process.Start("notepad.exe", SettingsPath);
-    private void OnApplySettingsNowMenuFlyoutItemClicked(object sender, RoutedEventArgs e) => ApplySettings();
+    private void OnOpenSettingsFileMenuFlyoutItemClicked(object sender, RoutedEventArgs e) => Process.Start("notepad.exe", SettingsPath);
     private void OnExitMenuFlyoutItemClicked(object sender, RoutedEventArgs e) => Environment.Exit(0);
     private void OnStartupProcessFlyoutItemClicked(object sender, RoutedEventArgs e)
     {
+        // Toggle the startup process
         if (StartupProcessHelper.IsStartupProcess) StartupProcessHelper.RemoveStartupProcess();
         else StartupProcessHelper.SetupStartupProcess();
+
+        // Update the text of the menu flyout item
         UpdateStartupProcessMenuFlyoutItemText();
     }
 
+    /// <summary>
+    /// This window is not supposed to be shown. Hide it when activated
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
     private void OnWindowActivated(object sender, WindowActivatedEventArgs args) => this.Hide();
+
+    /// <summary>
+    /// Gets the current status of the adaptive brightness feature
+    /// </summary>
+    /// <param name="isAC"></param>
+    /// <returns>True if adaptive brightness is enabled, false if it is disabled, and null if it is not available</returns>
+    private static bool? GetAdaptiveBrightnessStatus(bool isAC)
+    {
+        // Execute the powercfg command to get the current adaptive brightness settings.
+        string powercfgOutput = ExecutePowerCfgQuery();
+
+        // Determine the setting key based on the power supply status.
+        string settingKey = isAC ? " AC " : " DC ";
+
+        // Split the output into lines based on the environment's newline character.
+        var lines = powercfgOutput.Split(Environment.NewLine, StringSplitOptions.None);
+
+        // Iterate over each line to find one containing the "AC" or "DC" keyword.
+        foreach (var line in lines)
+        {
+            if (line.Contains(settingKey))
+            {
+                // Check if the line contains "0x00000000" or "0x00000001" and return the corresponding boolean value.
+                if (line.Contains("0x00000000"))
+                {
+                    return false;
+                }
+                else if (line.Contains("0x00000001"))
+                {
+                    return true;
+                }
+            }
+        }
+
+        // Return null if no matching setting is found.
+        return null;
+    }
+
+
+    /// <summary>
+    /// Executes the powercfg command to get the current adaptive brightness settings
+    /// </summary>
+    /// <returns>The output of the powercfg command</returns>
+    private static string ExecutePowerCfgQuery()
+    {
+        // Create a new process to run the powercfg command.
+        var process = new Process();
+        process.StartInfo.FileName = "powercfg";
+        process.StartInfo.Arguments = "/query SCHEME_CURRENT SUB_VIDEO ADAPTBRIGHT";
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+
+        // Start the process and read the output.
+        process.Start();
+        string output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        // Return the output of the powercfg command.
+        return output;
+    }
+
+    private static void SetAdaptiveBrightness(bool enable)
+    {
+        // AC Power
+        {
+            var query = "/setacvalueindex SCHEME_CURRENT SUB_VIDEO ADAPTBRIGHT";
+            var value = enable ? "1" : "0";
+            var queryProcess = new Process();
+            queryProcess.StartInfo.FileName = "powercfg";
+            queryProcess.StartInfo.Arguments = $"{query} {value}";
+            queryProcess.StartInfo.UseShellExecute = false;
+            queryProcess.StartInfo.CreateNoWindow = true;
+            queryProcess.Start();
+            queryProcess.WaitForExit();
+        }
+
+        // DC Power
+        {
+            var query = "/setdcvalueindex SCHEME_CURRENT SUB_VIDEO ADAPTBRIGHT";
+            var value = enable ? "1" : "0";
+            var queryProcess = new Process();
+            queryProcess.StartInfo.FileName = "powercfg";
+            queryProcess.StartInfo.Arguments = $"{query} {value}";
+            queryProcess.StartInfo.UseShellExecute = false;
+            queryProcess.StartInfo.CreateNoWindow = true;
+            queryProcess.Start();
+            queryProcess.WaitForExit();
+        }
+
+        // Apply the settings
+        var applyProcess = new Process();
+        applyProcess.StartInfo.FileName = "powercfg";
+        applyProcess.StartInfo.Arguments = "/S SCHEME_CURRENT";
+        applyProcess.StartInfo.UseShellExecute = false;
+        applyProcess.StartInfo.CreateNoWindow = true;
+        applyProcess.Start();
+        applyProcess.WaitForExit();
+    }
 }
---
+++ ScreenBrightnessByBattery/PowerConfigBrightnessController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ScreenBrightnessByBattery;
+
+public static partial class PowerConfigBrightnessController
+{
+    /// <summary>
+    /// The GUID of the subgroup that contains the brightness setting.
+    /// </summary>
+    private const string SubgroupGuid = "7516b95f-f776-4464-8c53-06167f40cc99";
+
+    /// <summary>
+    /// The GUID of the setting that controls the brightness value.
+    /// </summary>
+    private const string SettingGuid = "aded5e82-b909-4619-9949-f5d71dac0bcb";
+
+    /// <summary>
+    /// Retrieves the current brightness value for the specified power supply status.
+    /// </summary>
+    /// <param name="isAC">Indicates if the power supply status is AC.</param>
+    /// <returns>The current brightness value.</returns>
+    /// <exception cref="Exception">thrown when the brightness value cannot be retrieved.</exception>
+    public static int Get(bool isAC)
+    {
+        // Determine the setting key based on the power supply status.
+        string settingKey = isAC ? " AC " : " DC ";
+
+        // Create a new process to run the powercfg command.
+        var process = new Process();
+        process.StartInfo.FileName = "powercfg";
+        process.StartInfo.Arguments = $"/q SCHEME_CURRENT {SubgroupGuid} {SettingGuid}";
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+
+        // Start the process and read the output.
+        process.Start();
+        string output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        // Split the output into lines based on the environment's newline character.
+        var lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+        // Iterate over each line to find one containing the "AC" or "DC" keyword.
+        foreach (var line in lines)
+        {
+            if (line.Contains(settingKey))
+            {
+                var regex = BrightnessHexRegex();
+                var match = regex.Match(line);
+
+                return match.Success ? Convert.ToInt32(match.Value.Replace("0x", string.Empty), 16) : throw new Exception("Failed to retrieve the current brightness value.");
+            }
+        }
+
+        // If the brightness value cannot be retrieved, throw an exception.
+        throw new Exception("Failed to retrieve the current brightness value.");
+    }
+
+    /// <summary>
+    /// Sets the brightness value for both power supply statuses.
+    /// </summary>
+    /// <param name="value"></param>
+    public static void Set(int value)
+    {
+        Set(value, true); // AC Power
+        Set(value, false); // DC Power
+    }
+
+    /// <summary>
+    /// Sets the brightness value for the specified power supply status.
+    /// </summary>
+    /// <param name="value">The brightness value to set.</param>
+    /// <param name="isAC">Indicates if the power supply status is AC.</param>
+    public static void Set(int value, bool isAC)
+    {
+        // Set the brightness value for the specified power supply status.
+        var process = new Process();
+        process.StartInfo.FileName = "powercfg";
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+
+        if (isAC) process.StartInfo.Arguments = $"-SETACVALUEINDEX SCHEME_CURRENT {SubgroupGuid} {SettingGuid} {value}";
+        else process.StartInfo.Arguments = $"-SETDCVALUEINDEX SCHEME_CURRENT {SubgroupGuid} {SettingGuid} {value}";
+
+        process.Start();
+        process.WaitForExit();
+
+        // Apply the settings immediately
+        var applyProcess = new Process();
+        applyProcess.StartInfo.FileName = "powercfg";
+        applyProcess.StartInfo.Arguments = "/S SCHEME_CURRENT";
+        applyProcess.StartInfo.UseShellExecute = false;
+        applyProcess.StartInfo.CreateNoWindow = true;
+        applyProcess.Start();
+        applyProcess.WaitForExit();
+    }
+
+    /// <summary>
+    /// Source Generator Regex Method 
+    /// </summary>
+    /// <returns>Regex</returns>
+    [GeneratedRegex(@"0x[0-9a-fA-F]+")]
+    private static partial Regex BrightnessHexRegex();
+}
