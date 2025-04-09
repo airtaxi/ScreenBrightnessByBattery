8a77daaefeeffb753a89aa383a05d25b6bddc143
Howon Lee <kck4156@kagamine-rin.com>
2025-04-09 18:48:38 +0900

    Feature: check for updates

--- ScreenBrightnessByBattery/App.xaml.cs
+++ ScreenBrightnessByBattery/App.xaml.cs
@@ -1,26 +1,66 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
+using Microsoft.Windows.AppLifecycle;
+using Microsoft.Windows.AppNotifications;
+using Microsoft.Windows.AppNotifications.Builder;
+using ScreenBrightnessByBattery.Helpers;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ScreenBrightnessByBattery;
 
 public partial class App : Application
 {
+    private const string UpdateAvailableTitle = "Update Available";
+    private const int UpdateCheckIntervalInMinutes = 10;
+    private static readonly Timer UpdateCheckTimer;
+    private static readonly string VersionFilePath = Path.Combine(AppContext.BaseDirectory, "version");
+
+    static App() => UpdateCheckTimer = new(UpdateCheckTimerCallback, null, (int)TimeSpan.FromMinutes(UpdateCheckIntervalInMinutes).TotalMilliseconds, Timeout.Infinite);
+
+    private static async void UpdateCheckTimerCallback(object state) => await CheckForUpdateAsync();
+
+    private static async Task CheckForUpdateAsync()
+    {
+        try
+        {
+            var url = "https://raw.githubusercontent.com/airtaxi/ScreenBrightnessByBattery/master/latest";
+            var remoteVersionString = await HttpHelper.GetContentFromUrlAsync(url);
+            if (remoteVersionString is null) return;
+            else remoteVersionString = remoteVersionString.Trim();
+
+            var localVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            var remoteVersion = new Version(remoteVersionString);
+            if (localVersion >= remoteVersion) return;
+
+            string lastVersionString = null;
+            if (File.Exists(VersionFilePath))
+            {
+                lastVersionString = File.ReadAllText(VersionFilePath);
+                if (remoteVersionString == lastVersionString) return;
+            }
+
+            if (remoteVersionString != lastVersionString) File.WriteAllText(VersionFilePath, remoteVersionString);
+
+            var builder = new AppNotificationBuilder()
+                .AddText(UpdateAvailableTitle)
+                .AddText($"A new version ({remoteVersion}) is available.\nDo you want to download it?")
+                .AddArgument("versionString", remoteVersionString);
+
+            var notificationManager = AppNotificationManager.Default;
+            notificationManager.Show(builder.BuildNotification());
+        }
+        catch (HttpRequestException) { } // Ignore 
+        finally { UpdateCheckTimer.Change((int)TimeSpan.FromMinutes(UpdateCheckIntervalInMinutes).TotalMilliseconds, Timeout.Infinite); }
+    }
+
+    private Window _mainWindow;
+
     public App()
     {
         // Check for duplicated running instances.
@@ -39,9 +79,67 @@ public partial class App : Application
         InitializeComponent();
     }
 
-    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    protected override async void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
     {
-        var window = new MainWindow();
-        window.Activate();
+        AppNotificationManager notificationManager = AppNotificationManager.Default;
+        notificationManager.NotificationInvoked += OnNotificationManagerNotificationInvoked;
+        notificationManager.Register();
+
+        var activatedArgs = Microsoft.Windows.AppLifecycle.AppInstance.GetCurrent().GetActivatedEventArgs();
+        var activationKind = activatedArgs.Kind;
+
+        if (activationKind != ExtendedActivationKind.AppNotification) LaunchAndBringToForegroundIfNeeded();
+        else HandleNotification((AppNotificationActivatedEventArgs)activatedArgs.Data);
+
+        await CheckForUpdateAsync();
     }
+
+    private static void OnNotificationManagerNotificationInvoked(AppNotificationManager sender, AppNotificationActivatedEventArgs args) => HandleNotification(args);
+
+    private static void HandleNotification(AppNotificationActivatedEventArgs args)
+    {
+        var versionString = args.Arguments["versionString"];
+        if (versionString != null)
+        {
+            var url = "https://github.com/airtaxi/ScreenBrightnessByBattery/releases/tag/" + versionString;
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+    }
+
+    private void LaunchAndBringToForegroundIfNeeded()
+    {
+        if (_mainWindow == null)
+        {
+            _mainWindow = new MainWindow();
+            _mainWindow.Activate();
+        }
+        else
+        {
+            WindowHelper.ShowWindow(_mainWindow);
+        }
+    }
+
+    private static partial class WindowHelper
+    {
+        [LibraryImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [LibraryImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool SetForegroundWindow(IntPtr hWnd);
+
+        public static void ShowWindow(Window window)
+        {
+            // Bring the window to the foreground... first get the window handle...
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+
+            // Restore window if minimized... requires DLL import above
+            ShowWindow(hwnd, 0x00000009);
+
+            // And call SetForegroundWindow... requires DLL import above
+            SetForegroundWindow(hwnd);
+        }
+    }
+
 }
---
+++ ScreenBrightnessByBattery/Helpers/HttpHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ScreenBrightnessByBattery.Helpers;
+
+public static class HttpHelper
+{
+	private static readonly HttpClient SharedHttpClient = new();
+	public static async Task<string> GetContentFromUrlAsync(string url)
+	{
+		try
+		{
+			HttpResponseMessage response = await SharedHttpClient.GetAsync(url);
+			response.EnsureSuccessStatusCode();
+			return await response.Content.ReadAsStringAsync();
+		}
+		catch (HttpRequestException) { return null; }
+	}
+}
